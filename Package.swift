4db8514be0feb55398f15babd290ca7acf4ae6e8
Shaps Benkau <shapsuk@me.com>
2023-05-09 10:19:01 +0100

    Updates to latest SwiftUIBackports

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-         .package(url: "https://github.com/shaps80/SwiftUIBackports", from: "2.0.0"),
+         .package(url: "https://github.com/shaps80/SwiftUIBackports", from: "2.5.0"),
     ],
     targets: [
         .target(
